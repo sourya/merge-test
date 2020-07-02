f2dbf651be15a66cf05780bdbcaac47ebabb6f53
Sourya Vatsyayan <sourya@deepsource.io>
2020-07-02 12:14:26 +0530

    Add a.py
    Signed-off-by: Sourya Vatsyayan <sourya@deepsource.io>

---
+++ a.py
@@ -0,0 +1,15 @@
+import this
+
+print("123")
+
+def f():
+    raise NotImplemented
+    raise NotImplemented
+    raise NotImplemented
+    raise NotImplemented
+    raise NotImplemented
+    raise NotImplemented
+    raise NotImplemented
+    raise NotImplemented
+    raise NotImplemented
+    raise NotImplemented
